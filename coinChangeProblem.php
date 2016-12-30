83b2bc941aa7a5c3f94e29b13a07fc20f0597446
Fabio Costantino <fabcos92@gmail.com>
2016-12-30 01:55:32 +0100

    The Coin Change Problem solved on the Hackerrank

---
+++ coinChangeProblem.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+Dynamic Programming Solution to the Coin Changing Problem
+
+An optimal solution to finding all the making change for n cents using coins of denominations from coins list of length m.
+*/
+
+function waysOfChange($n, $m, $coins)
+{
+    $table = [];
+
+    for ($i = 0; $i < $m; $i++) {
+        $table[0][$i] = 1;
+    }
+
+    for ($i = 1; $i < $n + 1; $i++) {
+        for ($j = 0; $j < $m; $j++) {
+            if ($i - $coins[$j] >= 0) {
+                $x = $table[$i - $coins[$j]][$j];
+            } else {
+                $x = 0;
+            }
+            if ($j >= 1) {
+                $y = $table[$i][$j - 1];
+            } else {
+                $y = 0;
+            }
+
+            $table[$i][$j] = $x + $y;
+        }
+    }
+
+    return $table[$n][$m - 1];
+}
