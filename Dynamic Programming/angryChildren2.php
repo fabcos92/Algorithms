519212de445264c86322fbea5450cd95e70ede73
Fabio Costantino <fabcos92@gmail.com>
2017-01-02 21:13:21 +0100

    Angry Children 2

---
+++ Dynamic Programming/angryChildren2.php
@@ -0,0 +1,26 @@
+<?php
+function minUnfairness($packets, $children, $candies)
+{
+    $unfairness = $sum = 0;
+    $low = $candies[0];
+
+    for ($i = 1; $i < $children; $i++) {
+        $sum += $candies[$i];
+        $diff = abs($candies[$i] - $candies[$i - 1]);
+        $unfairness += $i * ($children - $i) * $diff;
+    }
+
+    $minUnfairness = $unfairness;
+
+    for ($i = $children; $i < $packets; $i++) {
+        $unfairness -= $sum - $low * ($children - 1);
+        $unfairness += $candies[$i] * ($children - 1) - $sum;
+
+        if ($unfairness < $minUnfairness) {
+          $minUnfairness = $unfairness;
+        }
+        $sum += $candies[$i] - $candies[$i - $children + 1];
+        $low = $candies[$i - $children + 1];
+    }
+    return $minUnfairness;
+}
